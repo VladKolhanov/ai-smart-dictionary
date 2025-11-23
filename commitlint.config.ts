566756701a8a29961c3cbd03f3079f539d1e07cf
Vlad Kolhanov <vladi.kolganov@gmail.com>
2025-11-23 19:12:54 +0200

    🚧 chore: remove commitlint and configure vscode commit message editor

--- commitlint.config.ts
+++
@@ -1,9 +0,0 @@
-import { type UserConfig } from '@commitlint/types'
-
-const Configuration: UserConfig = {
-  extends: ['@commitlint/config-conventional'],
-  formatter: '@commitlint/format',
-  rules: {},
-}
-
-export default Configuration
