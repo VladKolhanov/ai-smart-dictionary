c33898db77cf0ea70fcadd23a13ff188e1cb36c8
Vlad Kolhanov <vladi.kolganov@gmail.com>
2025-11-28 14:23:25 +0200

    ✨ feat: add db-deploy workflow for migrate database in production

--- src/app/[locale]/(home)/page.tsx
+++ src/app/[locale]/(home)/page.tsx
@@ -3,7 +3,6 @@ import { FormAddWord } from '@/ui/components/organisms/forms/form-add-word'
 export default function Home() {
   return (
     <div className="grid place-items-center">
-      Hello World!
       <FormAddWord />
     </div>
   )
